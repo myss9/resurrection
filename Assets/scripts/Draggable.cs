ca1235bbeadf704ddb0d7399d11e4c48c8f91384
myss9 <mase_k@1wotg.tk>
2016-04-14 23:19:52 +0300

    ДОбавил конечный префаб карты, готовую сцену с клетками поля, и два скрипта отрабатывающие перетягивание карты и попадание ее на определенные клетки

--- Assets/scripts/Draggable.cs
+++ Assets/scripts/Draggable.cs
@@ -4,12 +4,13 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-	Transform parentToReturnTo = null;
+	public Transform parentToReturnTo = null;
 
 	public void OnBeginDrag (PointerEventData evenData)
 	{
 		parentToReturnTo = this.transform.parent;
 		this.transform.SetParent (this.transform.parent.parent);
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
 	}
 
 	public void OnDrag (PointerEventData evenData)
@@ -20,5 +21,6 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 	public void OnEndDrag (PointerEventData evenData)
 	{
 		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
 	}
 }
--- Assets/scripts/DropZone.cs
+++ Assets/scripts/DropZone.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler {
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter (PointerEventData eventData)
+	{
+	}
+
+	public void OnPointerExit (PointerEventData eventData)
+	{
+	}
 
 	public void OnDrop (PointerEventData eventData)
 	{
-		
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable> ();
+		if (d != null) {
+			d.parentToReturnTo = this.transform;
+		}
 	}
+
+
 }
