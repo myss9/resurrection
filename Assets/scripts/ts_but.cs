2d43d10913de3966a516451a8bd393db9fc980c7
myss9 <mase_k@1wotg.tk>
2016-04-24 21:09:11 +0300

    добавил тестовую сцену с двумя кнопками. вроде как запилил чтобы по нажатию на кнопку что-то происходило. все работает через ТСРСоннектор. ну или вернее сказать. должно работать.

---
+++ Assets/scripts/ts_but.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+public class ts_but : MonoBehaviour {
+
+	private GameObject connector;
+
+	void Start(){
+		connector = GameObject.Find("TCPConnector");
+	}
+
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (150, 10, 100, 40), "Создать")) 
+		{
+			string json = "{\"action\":\"create\",\"data\":{\"string\":\"test\",\"int\":1}}";
+			connector.GetComponent<TCPConnector>().WriteString(json);
+		}
+		if (GUI.Button (new Rect (150, 50, 100, 40), "Закрыть")) 
+		{
+			string json = "{\"action\":\"close\",\"data\":{\"string\":\"test1\",\"int\":2}}";
+			connector.GetComponent<TCPConnector>().WriteString(json);
+		}
+		if (GUI.Button (new Rect (150, 400, 100, 40), "Да Ну Нахер")) 
+		{
+			Application.Quit();
+		}
+	}
+}
